fed11a4db1d3385e0e5162c355b1aa9afbb68807
legonate <legonate@gmail.com>
2018-11-26 19:00:59 -0500

    start

---
+++ pperson.c
@@ -0,0 +1,13 @@
+# include <iostream>
+using namespace std;
+
+class Person{
+  private:
+    string firstName;
+    string lastName;
+  
+  public:
+    Person(string, string)
+    string getName();
+    void operator<<();
+}
